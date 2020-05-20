7998dea3659b9bd179fe135c3be577909d87b1cb
Jessie Han <63132258+nkanyang@users.noreply.github.com>
2020-05-20 12:53:39 +1000

    Update git-prompt.sh

--- configs/gitbash/git-prompt.sh
+++ configs/gitbash/git-prompt.sh
@@ -12,7 +12,7 @@ else
         PS1='\[\033]0;GitBash:$PWD\007\]' # set window title
 #       PS1="$PS1"'\n'                 # new line
         PS1="$PS1"'\[\033[32m\]'       # change to green
-        PS1="$PS1"'\u@\h '             # user@host<space>
+        PS1="$PS1"'[\u@LAPTOP ~]'             # user@host<space>
 #       PS1="$PS1"'\[\033[35m\]'       # change to purple
 #       PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
 #       PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
@@ -32,7 +32,7 @@ else
                 fi
         fi
         PS1="$PS1"'\[\033[0m\]'        # change color
-        PS1="$PS1"'\]'                 # new line
+#       PS1="$PS1"']'                 # new line
         PS1="$PS1"'# '                 # prompt: always $
 fi
 
